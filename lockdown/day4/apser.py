50796e3cff273e27570d0f0e8b47b79a96e4bb0d
Shobhit Nigam <50029224+shobhit-nigam@users.noreply.github.com>
2020-05-05 08:05:04 +0530

    Add files via upload

---
+++ lockdown/day4/apser.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def red():
+    print("scarlet red")
+
+
+# In[2]:
+
+
+def blue():
+    print("cool blue")
+
+
+# In[3]:
+
+
+def magenta():
+    print("magenta, mixture of")
+    red()
+    blue()
+
+
+# In[ ]:
+
+
+
+
---
+++ lockdown/day4/karun.py
@@ -0,0 +1,12 @@
+"hello this is Karun"
+
+print("hello")
+
+def green():
+    print("go green")
+    
+data1 = 23
+data2 = 33
+
+def white():
+    print("kora kagaz")
